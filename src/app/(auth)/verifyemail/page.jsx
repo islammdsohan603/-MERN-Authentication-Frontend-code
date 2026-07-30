8d747394ecb4cb2f2b62b8b55aed858319f8afc5
islammdsohan603 <islammdsohan603@gmail.com>
2026-07-30 16:20:21 +0600

    verifyemail page added

--- src/app/(auth)/verifyemail/page.jsx
+++ src/app/(auth)/verifyemail/page.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 
 const VerifyMail = () => {
   return (
-    <div>
-      <h1>VerifyEmail</h1>
+    <div className="h-screen w-full relative overflow-hidden">
+      <div className=" min-h-screen flex items-center justify-center bg-green-100 p-4">
+        <div className="bg-white p-8 rounded-2xl shadow-lg w-full max-w-md text-center">
+          <h1 className="text-2xl font-semibold text-green-700 mb-4">
+            ✅ Check Your Email
+          </h1>
+          <p className=" text-gray-400 text-sm ">
+            We've sent you an email to verify your account. Pleass check your
+            inbox for email
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
